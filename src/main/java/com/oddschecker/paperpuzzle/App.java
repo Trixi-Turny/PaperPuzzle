4b1a53e523c59266c9e52bb58d2ce285bbc62709
Trixi Turny <trixi.turny@gmail.com>
2018-11-10 17:38:39 +0000

    Set up basic project with Maven

---
+++ src/main/java/com/oddschecker/paperpuzzle/App.java
@@ -0,0 +1,13 @@
+package com.oddschecker.paperpuzzle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
